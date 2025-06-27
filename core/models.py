e2c1babd007f321b6eac863a934fd6e08d901255
ThiagoORuby <trs@ic.ufal.br>
2025-06-27 08:10:57 -0300

    [feat](core): cria base para core (conexão com db)

---
+++ core/models.py
@@ -0,0 +1,26 @@
+
+from datetime import datetime
+from typing import Optional
+
+import sqlalchemy as sa
+from sqlalchemy.orm import Mapped, mapped_column, registry
+
+table_registry = registry()
+
+
+@table_registry.mapped_as_dataclass
+class Post:
+    __tablename__ = 'posts'
+
+    # init = False (campo não obrigatório na instanciação)
+    id: Mapped[int] = mapped_column(
+        init=False, primary_key=True, autoincrement=True
+    )
+    image_url: Mapped[Optional[str]]
+    title: Mapped[str]
+    url: Mapped[str]
+    description: Mapped[str]
+    date_published: Mapped[datetime] = mapped_column(
+        sa.DateTime(timezone=True)
+    )
+    type: Mapped[Optional[str]]
