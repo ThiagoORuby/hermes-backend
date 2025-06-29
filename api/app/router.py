e79e136292b64cf6375bc13444871b673f101def
ThiagoORuby <trs@ic.ufal.br>
2025-06-29 09:37:50 -0300

    [feat](api): criar schemas e rotas GET para posts, sources, subjects e stats

--- api/app/router.py
+++ api/app/router.py
@@ -1,10 +1,11 @@
+from datetime import date, timedelta
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
-from sqlalchemy import select
+from fastapi import APIRouter, Depends, Query
+from sqlalchemy import desc, func, select
 from sqlalchemy.orm import Session
 
-from api.app.schemas import PostList
+from app.schemas import FilterPost, ItemList, PostList, SourceCount, Stats, SubjectCount
 from core.database import get_session
 from core.models import Post
 
@@ -14,8 +15,79 @@ DBSession = Annotated[Session, Depends(get_session)]
 
 
 @router.get("/posts", response_model=PostList)
-def read_posts(session: DBSession):
+def read_posts(session: DBSession, filter: Annotated[FilterPost, Query()]):
+    # Filtra em titulo e descrição dos posts
+    q = filter.q or ""
+    query = select(Post).where(Post.title.contains(q) | Post.description.contains(q))
 
-    posts = session.scalars(select(Post)).all()
+    # Filtra por intervalo de tempo
+    if filter.start_date and filter.end_date:
+        query = query.where(
+            Post.date_published.between(filter.start_date, filter.end_date)
+        )
 
-    return {"data": posts}
+    # Ordena por coluna
+    if filter.order == "asc":
+        query = query.order_by(filter.sort_by)
+    else:
+        query = query.order_by(desc(filter.sort_by))
+
+    # Aplica paginação
+    offset = (filter.p - 1) * filter.pp
+    posts = session.scalars(query.offset(offset).limit(filter.pp))
+
+    return {"data": posts.all()}
+
+
+@router.get("/sources", response_model=ItemList)
+def read_sources(session: DBSession):
+    sources = session.scalars(select(Post.source).distinct())
+
+    return {"data": sources.all()}
+
+
+@router.get("/subjects", response_model=ItemList)
+def read_subjects(session: DBSession):
+    subjects = session.scalars(
+        select(Post.type).where(Post.type.is_not(None)).distinct()
+    )
+
+    return {"data": subjects.all()}
+
+
+@router.get("/stats", response_model=Stats)
+def read_stats(session: DBSession):
+    total_news = session.scalar(select(func.count(Post.id)))
+
+    today = date.today()
+    today_news = session.scalar(
+        select(func.count(Post.id)).where(
+            Post.date_published.between(today, today + timedelta(days=1))
+        )
+    )
+
+    news_by_subjects = (
+        session.execute(
+            select(func.count(Post.id), Post.type.label("subject"))
+            .where(Post.type.is_not(None))
+            .group_by(Post.type)
+        )
+        .mappings()
+        .all()
+    )
+
+    news_by_subjects = [SubjectCount.model_validate(data) for data in news_by_subjects]
+
+    news_by_sources = (
+        session.execute(select(func.count(Post.id), Post.source).group_by(Post.source))
+        .mappings()
+        .all()
+    )
+    news_by_sources = [SourceCount.model_validate(data) for data in news_by_sources]
+
+    return {
+        "total_news": total_news,
+        "today": today_news,
+        "news_by_subjects": news_by_subjects,
+        "news_by_sources": news_by_sources,
+    }
--- api/app/schemas.py
+++ api/app/schemas.py
@@ -1,9 +1,15 @@
-from datetime import datetime
+from datetime import date, datetime
+from typing import Literal, Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict, Field
+from pydantic.alias_generators import to_camel
 
 
 class PostOut(BaseModel):
+    model_config = ConfigDict(
+        alias_generator=to_camel, validate_by_alias=False, serialize_by_alias=True
+    )
+
     id: int
     url: str
     title: str
@@ -13,3 +19,38 @@ class PostOut(BaseModel):
 
 class PostList(BaseModel):
     data: list[PostOut]
+
+
+class ItemList(BaseModel):
+    data: list[str]
+
+
+class FilterPost(BaseModel):
+    p: int = Field(1, gt=0)
+    pp: int = Field(50, gt=0, le=50)
+    sort_by: str = "date_published"
+    order: Literal["desc", "asc"] = "desc"
+    q: str | None = None
+    start_date: Optional[date] = None
+    end_date: Optional[date] = None
+
+
+class SourceCount(BaseModel):
+    source: str
+    count: int
+
+
+class SubjectCount(BaseModel):
+    subject: str
+    count: int
+
+
+class Stats(BaseModel):
+    model_config = ConfigDict(
+        alias_generator=to_camel, validate_by_alias=False, serialize_by_alias=True
+    )
+
+    total_news: int
+    today: int
+    news_by_subjects: list[SubjectCount]
+    news_by_sources: list[SourceCount]
